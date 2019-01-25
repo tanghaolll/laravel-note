0758fe57d452d0a83f419b036557460e79106e94
tanghao <tanghaolll@163.com>
2019-01-25 16:50:34 +0800

    渲染模板

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    // 列表
+    public function index() {
+        return view("post/index");
+    }
+
+    // 详情页面
+    public function show() {
+        return view('post/show');
+    }
+
+    // 创建页面
+    public function create() {
+        return view('post/create');
+
+    }
+
+    // 创建逻辑
+    public function store() {
+
+    }
+
+    // 编辑页面
+    public function edit() {
+        return view('post/edit');
+    }
+
+    // 编辑逻辑
+    public function update() {
+
+    }
+
+    // 删除逻辑
+    public function delete() {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//文章列表
+Route::get('/posts', '\App\Http\Controllers\PostController@index');
+//创建文章
+Route::get('/posts/create','\App\Http\Controllers\PostController@create');
+Route::post('/posts','\App\Http\Controllers\PostController@store');
+//文章详情页
+Route::get('/posts/{port}','\App\Http\Controllers\PostController@show');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//编辑文章
+Route::get('/posts/{port}/edit','\App\Http\Controllers\PostController@edit');
+Route::put('/posts/{port}','\App\Http\Controllers\PostController@update');
+//删除文章
+Route::get('/posts/delete','\App\Http\Controllers\PostController@delete');
+
+
+
